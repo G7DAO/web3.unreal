94f9402e4857f165462ff8623b4f6f97b24a3e43
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-01-07 14:03:53 -0800

    add keccak256 cpp class, bp node, and unit test

---
+++ Plugins/web3Unreal/Source/web3Unreal/Private/Keccak256Node.cpp
@@ -0,0 +1,27 @@
+#include "Keccak256Node.h"
+#include "Keccak256.h"
+
+std::string UKeccak256Node::hexStr(unsigned char* data, int len)
+{
+	std::string s(len * 2, ' ');
+	for (int i = 0; i < len; ++i) {
+		s[2 * i] = hexmap[(data[i] & 0xF0) >> 4];
+		s[2 * i + 1] = hexmap[data[i] & 0x0F];
+	}
+	return s;
+}
+
+FString UKeccak256Node::Keccak256(FString data) {
+	auto dataLengthInBytes = data.Len();
+	std::string msg;
+	for (auto iter = data.begin(); iter != data.end(); ++iter) {
+		TCHAR char_i = (unsigned char) *iter;
+		msg.push_back(char_i);
+	}
+	uint8_t dest[32] = { 0 };
+	const uint8_t* msgUint8 = (uint8_t*)msg.c_str();
+	Keccak256::getHash(msgUint8, msg.length(), dest);
+	std::string retHex = "0x" + hexStr(dest, 32);
+	FString ret(retHex.c_str());
+	return ret;
+}
---
+++ Plugins/web3Unreal/Source/web3Unreal/Public/Keccak256Node.h
@@ -0,0 +1,20 @@
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Keccak256Node.generated.h"
+
+#pragma once
+
+/**
+ *
+ */
+UCLASS()
+class UKeccak256Node : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Web3.Unreal")
+		static FString Keccak256(FString data);
+
+private:
+	static std::string hexStr(unsigned char* data, int len);
+	static constexpr char hexmap[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+						   '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+};
