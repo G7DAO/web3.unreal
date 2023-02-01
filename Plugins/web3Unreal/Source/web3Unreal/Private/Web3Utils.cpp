47795489ca2054c6e40c3c8ce264d1b8739de234
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-02-01 15:40:06 -0800

    add ecdsa signing and unit test

--- Plugins/web3Unreal/Source/web3Unreal/Private/Web3Utils.cpp
+++ Plugins/web3Unreal/Source/web3Unreal/Private/Web3Utils.cpp
@@ -13,13 +13,18 @@ std::string UWeb3Utils::hexStr(unsigned char* data, int len)
 	return s;
 }
 
+std::string UWeb3Utils::hexStr(std::vector<unsigned char> data)
+{
+	return UWeb3Utils::hexStr(&data[0], data.size());
+}
+
 /*
  * Converts a string with hex chars representing a hex value to its corresponding byte array
  */
-void UWeb3Utils::ByteArrayFromHexStr(FString hexString, unsigned char byteArray[])
+std::vector<unsigned char> UWeb3Utils::ByteArrayFromHexStr(FString hexString)
 {
 	FString hexStringUpper = hexString.ToUpper();
-	int byteArrayIndex = 0;
+	std::vector<unsigned char> byteArray;
 	for (auto iter = hexStringUpper.begin(); iter != hexStringUpper.end(); ++iter)
 	{
 		wchar_t char_i = *iter;
@@ -34,9 +39,9 @@ void UWeb3Utils::ByteArrayFromHexStr(FString hexString, unsigned char byteArray[
 		auto byte_0 = hexCharToByteMap.at(char_i);
 		auto byte_1 = hexCharToByteMap.at(char_iPlus1);
 		unsigned char byteToAdd = (byte_0 << 4) | byte_1;
-		byteArray[byteArrayIndex] = byteToAdd;
-		++byteArrayIndex;
+		byteArray.push_back(byteToAdd);
 	}
+	return byteArray;
 }
 /*
  * Converts TArray of bytes to c++ string representing the byte array in hex
--- Plugins/web3Unreal/Source/web3Unreal/Public/Web3Utils.h
+++ Plugins/web3Unreal/Source/web3Unreal/Public/Web3Utils.h
@@ -14,7 +14,30 @@ public:
 		static FString ByteArrayToFString(TArray<uint8> arr);
 
 	static std::string hexStr(unsigned char* data, int len);
-	static void ByteArrayFromHexStr(FString hexString, unsigned char* byteArray);
+	static std::string hexStr(std::vector<unsigned char> data);
+	static std::vector<unsigned char> ByteArrayFromHexStr(FString hexString);
+
+	template< typename T>
+	static TArray<T> ConvertVectorToTArray(std::vector<T> vec){
+		TArray<T> arr;
+		arr.SetNumUninitialized(vec.size());
+
+		for(int i=0; i< vec.size(); i++){
+			arr[i] = vec[i];
+		}
+		return arr;
+	}
+	
+	template< typename T>
+	static std::vector<T> ConvertTArrayToVector(TArray<T> byteArray){
+		std::vector<T> byteVector;
+		for (auto iter = byteArray.begin(); iter != byteArray.end(); ++iter)
+		{
+			T char_i = *iter;
+			byteVector.push_back(char_i);
+		}
+		return byteVector;
+	}
 private:
 	static constexpr char hexmap[] = { '0', '1', '2', '3', '4', '5', '6', '7',
 						   '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
