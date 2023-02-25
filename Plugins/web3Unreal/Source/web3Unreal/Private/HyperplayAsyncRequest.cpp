d4970565ff92a0872704c8d9463eada5218bcae1
iratinho <iratinho.dev@gmail.com>
2023-02-25 22:20:42 +0000

    Create base class for async requests

---
+++ Plugins/web3Unreal/Source/web3Unreal/Private/HyperplayAsyncRequest.cpp
@@ -0,0 +1,44 @@
+﻿#include "HyperplayAsyncRequest.h"
+
+namespace
+{
+	bool StatusCodeIsSuccess(int32 statusCode)
+	{
+		if (statusCode > 199 && statusCode < 300)
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
+void UHyperplayAsyncRequest::OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	UE_LOG(LogTemp, Warning, TEXT("bWasSuccessful = %hs"), bWasSuccessful ? "true" : "false");
+	
+	if (bWasSuccessful) {
+		const int32 statusCode = Response->GetResponseCode();
+		if (!StatusCodeIsSuccess(statusCode))
+		{
+			this->OnFailure.Broadcast();
+			return;
+		}
+		
+		const FString ResponseText = Response->GetContentAsString();
+
+		// JSON parsing
+		TSharedPtr<FJsonValue> ParsedJSON;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseText);
+		FJsonSerializer::Deserialize(Reader, ParsedJSON);
+		this->ProcessResponse(Response, statusCode);
+
+		return;
+	}
+	
+	this->OnFailure.Broadcast();
+}
+
+void UHyperplayAsyncRequest::ProcessResponse(FHttpResponsePtr Response, int32 statusCode)
+{
+}
+
---
+++ Plugins/web3Unreal/Source/web3Unreal/Private/HyperplayAsyncRequest.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "HyperplayAsyncRequest.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FFailedOutputPin);
+
+UCLASS()
+class UHyperplayAsyncRequest : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	virtual void ProcessResponse(FHttpResponsePtr Response, int32 statusCode);
+
+private:
+	UPROPERTY(BlueprintAssignable, meta=(AllowPrivateAccess=true))
+	FFailedOutputPin OnFailure;
+};
