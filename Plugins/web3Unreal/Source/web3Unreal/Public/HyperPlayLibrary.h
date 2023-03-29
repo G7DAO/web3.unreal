7e5d89a642f3d9a98fae982b52500342fa3ec894
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-03-29 11:56:14 -0700

    change UHyperPlayLibrary to regular c++ class

--- Plugins/web3Unreal/Source/web3Unreal/Public/HyperPlayLibrary.h
+++ Plugins/web3Unreal/Source/web3Unreal/Public/HyperPlayLibrary.h
@@ -1,18 +1,12 @@
 #pragma once
 
-#include "Kismet/BlueprintAsyncActionBase.h"
 #include "Json.h"
-#include "Runtime/Online/HTTP/Public/Http.h"
 #include "HyperPlayLibrary.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(HyperPlayLibraryLog, Display, All);
 
-UCLASS()
-class UHyperPlayLibrary : public UBlueprintAsyncActionBase
+class UHyperPlayLibrary
 {
-	GENERATED_BODY()
-
 public:
 	static TSharedPtr<FJsonValue> CreateJsonValue(FString obj);
-
 };
