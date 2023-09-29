33249f0845131750bd7f52bb56c9fddeca55a360
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-09-28 19:52:41 -0700

    add libsecp256k1 mac x86 and arm64 libs

--- Source/web3Unreal/web3Unreal.Build.cs
+++ Source/web3Unreal/web3Unreal.Build.cs
@@ -2,6 +2,7 @@
 
 using UnrealBuildTool;
 using System.IO;
+using System.Collections.Generic;
 
 public class web3Unreal : ModuleRules
 {
@@ -9,44 +10,52 @@ public class web3Unreal : ModuleRules
 	{
 		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/")); }
 	}
-	private string GetSecpLibraryPath(ReadOnlyTargetRules Target)
+	private List<string> GetSecpLibraryPath(ReadOnlyTargetRules Target)
 	{
+		List<string> secpLibs = new List<string>();
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			return Path.Combine(ThirdPartyPath,"lib", "libsecp256k1.lib");
+			secpLibs.Add(Path.Combine(ThirdPartyPath, "lib", "libsecp256k1.lib"));
 		}
 
 		if (Target.Platform == UnrealTargetPlatform.Linux)
 		{
-			return Path.Combine(ThirdPartyPath,"lib", "libsecp256k1.a");
+			secpLibs.Add(Path.Combine(ThirdPartyPath, "lib", "libsecp256k1.a"));
 		}
 
 		if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
-			return Path.Combine(ThirdPartyPath,"lib", "libsecp256k1.a");
+			if (Target.Architectures.ToString().Contains("x64"))
+			{
+				secpLibs.Add(Path.Combine(ThirdPartyPath, "lib", "libsecp256k1_mac_x86_64.a"));
+			}
+			if (Target.Architectures.ToString().Contains("arm64"))
+			{
+				secpLibs.Add(Path.Combine(ThirdPartyPath, "lib", "libsecp256k1_mac_arm64.a"));
+			}
 		}
 
-		return null;
+		return secpLibs;
 	}
 	public web3Unreal(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
 			new string[] {
 				// ... add public include paths required here ...
 				Path.Combine(ThirdPartyPath,"include")
 			}
 			);
-				
-		
+
+
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
 			}
 			);
-			
-		
+
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -57,8 +66,8 @@ public class web3Unreal : ModuleRules
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
-			
-		
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -69,24 +78,20 @@ public class web3Unreal : ModuleRules
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
-		
-		
+
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
-		
-		
-		//PublicAdditionalLibraries.Add("libsecp256k1.a");
-		//PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory,"lib", "libsecp256k1.a"));
-		//PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory,"Public", "libsecp256k1.a"));
-		string libraryPath = GetSecpLibraryPath(Target);
 
-		if (libraryPath != null)
+		List<string> libraryPaths = GetSecpLibraryPath(Target);
+
+		foreach (string secpPath in libraryPaths)
 		{
-			PublicAdditionalLibraries.Add(libraryPath);
+			PublicAdditionalLibraries.Add(secpPath);
 		}
 	}
 }
